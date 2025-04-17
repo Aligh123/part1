16106414f6bce57f56ad7cf8860545eb6d7e58c8
aligh123 <fanava.itnovin@yahoo.com>
2025-04-17 00:33:47 -0700

    Added exercise1.2 - Refactored course data to object with array

---
+++ exercise1.2/src/Total.jsx
@@ -0,0 +1,10 @@
+function Total({ parts }) {
+  const total = parts[0].exercises + parts[1].exercises + parts[2].exercises;
+  return (
+    <div>
+      <p>Number of exercises {total}</p>
+    </div>
+  );
+}
+
+export default Total;
