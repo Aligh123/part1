646b6345f1fe309814a0297f90b1ab350b06e179
aligh123 <fanava.itnovin@yahoo.com>
2025-04-17 02:19:29 -0700

    Exercise 1.5 - Use reduce to calculate total exercises

---
+++ exercise1.5/src/Total.jsx
@@ -0,0 +1,8 @@
+const Total = ({ parts }) => {
+    const total = parts.reduce((sum, part) => sum + part.exercises, 0);
+  
+    return <p><strong>Total number of exercises {total}</strong></p>;
+  };
+  
+  export default Total;
+  
