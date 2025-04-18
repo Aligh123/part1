1c0ab536d5491d959d3ca3d56113cf6539e272f0
aligh123 <fanava.itnovin@yahoo.com>
2025-04-18 07:45:33 -0700

    Exercise 1.10 : Extract Button and StatisticLine components

---
+++ 1.10-unicafe/src/Statistics.jsx
@@ -0,0 +1,22 @@
+import StatisticLine from "./StatisticLine";
+
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad;
+  if (total === 0) {
+    return <p>No feedback given</p>;
+  }
+  const average = (good - bad) / total;
+  const positive = (good / total) * 100;
+
+  return (
+    <div>
+      <StatisticLine text="good" value={good} />
+      <StatisticLine text="neutral" value={neutral} />
+      <StatisticLine text="bad" value={bad} />
+      <StatisticLine text="all" value={total} />
+      <StatisticLine text="average" value={average.toFixed(1)} />
+      <StatisticLine text="positive" value={`${positive.toFixed(1)} %`} />
+    </div>
+  );
+};
+export default Statistics;
